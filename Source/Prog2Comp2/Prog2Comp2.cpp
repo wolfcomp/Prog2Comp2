437516a563b86f1fcee013144256eddd1ac564c7
wolfcomp <magnus1997.mho@gmail.com>
2023-02-09 10:03:25 +0100

    Initial commit unreal

---
+++ Source/Prog2Comp2/Prog2Comp2.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Prog2Comp2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Prog2Comp2, "Prog2Comp2" );
---
+++ Source/Prog2Comp2/Prog2Comp2GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Prog2Comp2GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROG2COMP2_API AProg2Comp2GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
